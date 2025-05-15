bfc8a8d4b43a26aa0677d88003e4c81d28290f9b
Marvin Kadoyo <kadoyomarvin@gmail.com>
2025-05-15 08:30:38 +0300

    Implement Fibonacci sequence printer in print_fibonacci function

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,12 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    fibonacci = []
+    for i in range(length):
+        if i == 0:
+            fibonacci.append(0)
+        elif i == 1:
+            fibonacci.append(1)
+        else:
+            fibonacci.append(fibonacci[i - 1] + fibonacci[i - 2])
+    print(fibonacci)
